8ab4610db4a1508fde1e4d8a0be3c9b6b0a40f73
Jon Sherrard <jon@andthats.it>
2016-10-20 14:29:36 +0100

    fix autoload bug

--- plugin.php
+++ plugin.php
@@ -11,6 +11,17 @@
 * License: GPL2
 */
 
+if (file_exists(__DIR__ . '/../../../../vendor/getherbert/')) {
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
+} else {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+if (file_exists(__DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php')) {
+    require_once __DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php';
+} else {
+    require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
 

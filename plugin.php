a903fe52400f0c7f0d1fde478a9b96d1c2cc06cb
Jon Sherrard <jon@andthats.it>
2016-10-20 15:31:35 +0100

    fix the plugin

--- plugin.php
+++ plugin.php
@@ -22,9 +22,6 @@ if (file_exists(__DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/au
     require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
-
 use add_action;
 use TimberPost;
 use AgreableWidgetService;
